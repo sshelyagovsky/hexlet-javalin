d63653e70e2d47f509103ab30560b1664944aeb0
s.shelyagovsky <s.shelyagovsky@gmail.com>
2024-06-27 22:54:48 +0300

    javalin handling param

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,9 +18,6 @@ repositories {
 }
 
 dependencies {
-    // ¬ерсии зависимостей могут отличатьс€
-    // «десь мы сразу подключаем зависимости,
-    // которые понадоб€тс€ во врем€ обучени€
     implementation("io.javalin:javalin:6.1.3")
     implementation("org.slf4j:slf4j-simple:2.0.7")
     implementation("io.javalin:javalin-rendering:6.1.3")
--- src/main/java/org/example/hexlet/HelloWorld.java
+++ src/main/java/org/example/hexlet/HelloWorld.java
@@ -5,10 +5,22 @@ import io.javalin.Javalin;
 public class HelloWorld {
     public static void main(String[] args) {
 
+//        var app = Javalin.create(config -> {
+//            config.bundledPlugins.enableDevLogging();
+//        });
+//        app.get("/", ctx -> ctx.result("Hello World"));
+//        app.start(7070);
         var app = Javalin.create(config -> {
             config.bundledPlugins.enableDevLogging();
         });
-        app.get("/", ctx -> ctx.result("Hello World"));
+        app.get("/users", ctx -> ctx.result("GET /users"));
+        app.post("/users", ctx -> ctx.result("POST /users"));
+        app.get("/hello", ctx -> {
+            var name  = ctx.queryParamAsClass("name", String.class).getOrDefault("Hello, World!");
+            ctx.result(name);
+        });
         app.start(7070);
+
+
     }
 }
